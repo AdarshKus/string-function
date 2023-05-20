e271f5614b4a22537d693fc3167d1c00b85b9d45
AdarshKus <106098727+AdarshKus@users.noreply.github.com>
2023-05-20 17:21:10 +0530

    Add files via upload

---
+++ String_function.py
@@ -0,0 +1,9 @@
+story = "in a small village surrounded by a lush forest, a young girl named Lily embarked on a daring quest to find a hidden treasure. Overcoming obstacles and guided by woodland creatures, Lily discovered a shimmering waterfall. Reflecting on girl's journey, she realized that the true treasure was the personal growth she experienced. girl's story inspired others to embrace their dreams and appreciate the journey. The village thrived, forever transformed by the spirit of adventure and the power of dreams"
+
+print(len(story))
+print(story.endswith("dreams"))
+print(story.count("a"))
+print(story.count("an"))
+print(story.capitalize())
+print(story.find("girl"))
+print(story.replace("girl","boy"))
